4323cd88c470f885b1df5dd8e86827cd7a9ba78a
yasuperu <info@jasperkamphuis.com>
2026-03-08 15:35:39 +0100

    reroute homepage

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/kokenmetjasper" element={<Home />} />
         <Route path="/recipe/:id" element={<RecipePage />} />
       </Routes>
     </Router>
